b180d1c6fcd3197f73733b2f88f0ea2b56755a86
minhtungdoan <minhtungdoan.work@gmail.com>
2024-01-29 17:50:14 +0700

    add ngx-pagination

--- src/app/pages/admin/users/users.component.ts
+++ src/app/pages/admin/users/users.component.ts
@@ -3,11 +3,12 @@ import { DatePipe, NgFor, NgIf } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
 import { User } from '../../../types/Auth';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [NgFor, RouterLink, NgIf, DatePipe],
+  imports: [NgFor, RouterLink, NgIf, DatePipe, NgxPaginationModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css',
 })
@@ -16,10 +17,22 @@ export class UsersComponent {
   loading: boolean = true;
   userList: User[] = [];
 
+  p: number = 1;
+  total: number = 0;
+
   ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(): void {
     this.userService.getUserList().subscribe((res: User[]) => {
       this.userList = res;
       this.loading = false;
     });
+  }
+
+  pageChangeEvent(event: number) {
+    this.p = event;
+    this.getUsers();
   }
 }
